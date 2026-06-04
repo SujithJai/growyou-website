6e0c07f7b093d7761a6d2ae06f2a9d9a716ea66a
SujithJai <sujithjai007@gmail.com>
2026-06-05 02:38:37 +0530

    Add Microsoft Clarity tracking

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,19 @@ script2.innerHTML = `
 `;
 document.head.appendChild(script2);
 
+// Microsoft Clarity
+const clarityScript = document.createElement("script");
+clarityScript.innerHTML = `
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "x1y5we07kf");
+`;
+document.head.appendChild(clarityScript);
+
 createRoot(document.getElementById("root")!).render(
+
   <StrictMode>
     <App />
   </StrictMode>

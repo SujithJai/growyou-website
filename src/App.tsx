b10f5c148cea8bdc81bc634496f93a661461acc9
SujithJai <sujithjai007@gmail.com>
2026-06-05 12:20:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -923,7 +923,7 @@ export default function App() {
         <div className="flex items-center justify-between gap-4">
          <a href="#hero" className="flex items-center gap-3">
 <motion.img
-  src="/logo.png"
+  src="/logo png.png"
   alt="GroYou Logo"
   className="h-14 w-auto"
   animate={{
